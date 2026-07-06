61da1c06ba1c357a66a2298e1ead611d8d33990e
rodrigue-betscope <lindadady910@gmail.com>
2026-07-06 13:55:01 +0100

    Update match details and odds in app.py

--- app.py
+++ app.py
@@ -70,16 +70,16 @@ elif menu == "👑 VIP Privé":
             # ✍️ MODIFIE UNIQUEMENT CES COTES CHAQUE MATIN
             
             # MATCH 1 : SCORE EXACT
-            match_1 = "Real Madrid vs Barcelone"
-            cote_1_M1 = 1.85   
-            cote_X_M1 = 3.60   
-            cote_2_M1 = 3.90   
+            match_1 = "Portugal vs Espagne"
+            cote_1_M1 = 4.125  
+            cote_X_M1 = 3.69 
+            cote_2_M1 = 1.991
             
             # MATCH 2 : MI-TEMPS / FIN DE MATCH (HT/FT)
-            match_2 = "Liverpool vs Chelsea"
-            cote_1_M2 = 1.45   
-            cote_X_M2 = 4.50   
-            cote_2_M2 = 6.00   
+            match_2 = "états Unis vs Belgique"
+            cote_1_M2 = 2.589  
+            cote_X_M2 = 3.505  
+            cote_2_M2 = 2.909  
             
             # --- CALCULS IA ---
             p1_raw, px_raw, p2_raw = 1/cote_1_M1, 1/cote_X_M1, 1/cote_2_M1
