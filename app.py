ad667c3be730cf2899dc47bef09e2d002874bcf0
rodrigue-betscope <lindadady910@gmail.com>
2026-07-12 08:44:34 +0100

    Update VIP access key check to empty string
    Changed access key condition to an empty string.

--- app.py
+++ app.py
@@ -32,7 +32,7 @@ elif menu == "👑 VIP":
     
     cle_acces = st.text_input("🔑 Entrez votre clé d'accès VIP :", type="password")
     
-    if cle_acces == "DADY2026":
+    if cle_acces == "":
         st.success("🔓 Accès VIP accordé.")
         st.write("Collez le lien d'un match ci-dessous. L'algorithme analyse instantanément la tendance, le type de compétition, la forme et les absences.")
         
