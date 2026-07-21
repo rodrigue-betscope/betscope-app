bb1c399f48ec7e99a7e14c57d67cf1108d080282
rodrigue-betscope <lindadady910@gmail.com>
2026-07-21 16:45:48 +0100

    Create data preparation function in prediction_engine
    Add data preparation function for teams

---
+++ prediction_engine.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import numpy as np
+import scipy
+
+def preparer_donnees(t1, t2):
+    # Module de traitement et de calcul statistique additionnel
+    return {
+        "Equipe 1": t1,
+        "Equipe 2": t2
+    }
