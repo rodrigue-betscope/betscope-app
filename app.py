bca455ea8dbbb3674650b85edb05df69276e84a6
rodrigue-betscope <lindadady910@gmail.com>
2026-08-05 13:29:35 +0100

    Fix formatting issues in app.py

--- app.py
+++ app.py
@@ -164,7 +164,7 @@ elif menu == "⚡ Moteur Quantique VIP v3.0":
                     prob_under_25 += matrice_probabilités[i, j]
         prob_over_25 = max(0.0, 1.0 - prob_under_25)
 
-        prob_btts_non = np.sum(matrice_probabilités[0, :]) + np.sum(matrice_probabilités[:, 0]) - matrice_probabilités[0,0]
+        prob_btts_non = np.sum(matrice_probabilités[0, :]) + np.sum(matrice_probabilités[:, 0]) - matrice_probabilités[0, 0]
         prob_btts_oui = max(0.0, 1.0 - prob_btts_non)
 
         index_max = np.unravel_index(np.argmax(matrice_probabilités), matrice_probabilités.shape)
@@ -178,5 +178,5 @@ elif menu == "⚡ Moteur Quantique VIP v3.0":
         
         col_res1, col_res2, col_res3 = st.columns(3)
         with col_res1:
-            st.markdown(f'<div class="metric-box">🏆 Victoire Locale <b>{nom_dom} (1)</b><span class="highlight-value text-glow">{prob_1*100:.2f}%</span></div>', autocomplete=None, unsafe_allow_html=True)
+            st.markdown(f'<div class="metric-box">🏆 Victoire Locale <b>{nom_dom} (1)</b><span class="highlight-value text-glow">{prob_1*100:.2f}%</span></div>', unsafe_allow_html=True)
         with col_res2:
